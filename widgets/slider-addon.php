c595e0ed8a1deb65a5186686d23bc69c3089caa7
chronob <finn.o.bayer@gmail.com>
2021-06-02 11:56:34 +0200

    added content controls without functionality

--- widgets/slider-addon.php
+++ widgets/slider-addon.php
@@ -4,6 +4,8 @@ namespace Elementor_Slider_Addon\Widgets;
 
 use Elementor\Repeater;
 use Elementor\Widget_Base;
+use ElementorPro\Modules\QueryControl\Controls\Group_Control_Related;
+use Elementor\Controls_Manager;
 
 if (! defined('ABSPATH')) {
     exit;
@@ -38,11 +40,62 @@ class Elementor_Slider_Addon extends Widget_Base
         $this->start_controls_section(
             'content_section',
             [
-                'label' => __('Options', self::$slug),
+                'label' => __('Content Type', self::$slug),
                 'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
             ]
         );
 
+        //Selector if the content is static or should come from a query
+        $this->add_control(
+            'slide-content',
+            [
+                'label' => __('Slider Content', self::$slug),
+                'type' => \Elementor\Controls_Manager::SELECT,
+                'default' => 'static',
+                'options' => [
+                    'static' => __('Static', self::$slug),
+                    'query' => __('Query', self::$slug),
+                ],
+            ],
+        );
+        $this->add_responsive_control(
+			'number-slides',
+			[
+				'label' => __( 'Number of Slides', self::$slug ),
+				'type' => Controls_Manager::SELECT,
+				'default' => '3',
+				'tablet_default' => '2',
+				'mobile_default' => '1',
+				'options' => [
+					'1' => '1',
+					'2' => '2',
+					'3' => '3',
+					'4' => '4',
+					'5' => '5',
+					'6' => '6',
+				],
+				/*
+                TODO: Add stuff here to add functionality! -> this code is copied from the portfolio of elementor pro
+                'prefix_class' => 'elementor-grid%s-',
+				'frontend_available' => true,
+				'selectors' => [
+					'.elementor-msie {{WRAPPER}} .elementor-portfolio-item' => 'width: calc( 100% / {{SIZE}} )',
+				],*/
+			]
+		);
+        $this->end_controls_section();
+        
+        $this->start_controls_section(
+            'static_content_section',
+            [
+                'label' => __('Static Content', self::$slug),
+                'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+                'condition' => [
+                    'slide-content' => 'static'
+                ]
+            ]
+        );
+        /* TODO: Add the repeater for the static slides.
         // Use the repeater to define one one set of the items we want to repeat look like
         $repeater = new Repeater();
 
@@ -89,7 +142,153 @@ class Elementor_Slider_Addon extends Widget_Base
                 ],
                 'title_field' => '{{{ option_value }}}'
             ]
+        );*/
+        $this->end_controls_section();
+        
+        $this->start_controls_section(
+            'query_content_section',
+            [
+                'label' => __('Query', self::$slug),
+                'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+                'condition' => [
+                    'slide-content' => 'query'
+                ]
+            ]
         );
+
+		$this->add_group_control(
+			Group_Control_Related::get_type(),
+			[
+				'name' => 'posts',
+				'presets' => [ 'full' ],
+				'exclude' => [
+					'posts_per_page', //use the one from Layout section
+				],
+			]
+		);
+
+		$this->end_controls_section();
+
+        $this->start_controls_section(
+            'navigation_content_section',
+            [
+                'label' => __('Navigation', self::$slug),
+                'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+            ]
+        );
+        $this->add_control(
+            'navigation_show',
+            [
+                'label' => __( 'Show', self::$slug ),
+				'type' => \Elementor\Controls_Manager::SWITCHER ,
+                'label_on' => __( 'Show', self::$slug ),
+				'label_off' => __( 'Hide', self::$slug ),
+				'return_value' => 'yes',
+				'default' => 'yes',
+            ]
+        );
+        $this->add_control(
+			'navigation_position',
+			[
+				'label' => __( 'Position', self::$slug ),
+				'type' => \Elementor\Controls_Manager::CHOOSE,
+				'options' => [
+					'left' => [
+						'title' => __( 'Above', self::$slug ),
+						'icon' => 'fa fa-align-left',
+					],
+					'center' => [
+						'title' => __( 'Around', self::$slug ),
+						'icon' => 'fa fa-align-center',
+					],
+					'right' => [
+						'title' => __( 'Below', self::$slug ),
+						'icon' => 'fa fa-align-right',
+					],
+				],
+                'condition' => [
+                    'navigation_show' => 'yes'
+                ],
+				'default' => 'center',
+				'toggle' => true,
+			]
+		);
+        $this->add_control(
+			'navigation_alignment',
+			[
+				'label' => __( 'Alignment', self::$slug ),
+				'type' => \Elementor\Controls_Manager::CHOOSE,
+				'options' => [
+					'left' => [
+						'title' => __( 'Left', self::$slug ),
+						'icon' => 'fa fa-align-left',
+					],
+					'center' => [
+						'title' => __( 'Center', self::$slug ),
+						'icon' => 'fa fa-align-center',
+					],
+					'right' => [
+						'title' => __( 'Right', self::$slug ),
+						'icon' => 'fa fa-align-right',
+					],
+				],
+                'conditions' => [
+                    'relation' => 'and',
+                    'terms' => [
+                        [
+                            'name' => 'navigation_show',
+                            'operator' => '==',
+                            'value' => 'yes'
+                        ],
+                        [
+                            'name' => 'navigation_position',
+                            'operator' => '!=',
+                            'value' => 'center'
+                        ]
+                    ]
+                ],
+				'default' => 'center',
+				'toggle' => true,
+			]
+		);
+        $this->add_control(
+			'icon-prev',
+			[
+				'label' => __( 'Previous Icon', self::$slug ),
+				'type' => Controls_Manager::ICONS,
+				'default' => [
+					'value' => 'fas fa-arrow-left',
+					'library' => 'fa-solid',
+				],
+                'condition' => [
+                    'navigation_show' => 'yes'
+                ],
+				'skin' => 'inline',
+				'label_block' => false,
+				'frontend_available' => true,
+			]
+		);
+        $this->add_control(
+			'icon-next',
+			[
+				'label' => __( 'Next Icon', self::$slug ),
+				'type' => Controls_Manager::ICONS,
+				'default' => [
+					'value' => 'fas fa-arrow-right',
+					'library' => 'fa-solid',
+				],
+                'condition' => [
+                    'navigation_show' => 'yes'
+                ],
+				'skin' => 'inline',
+				'label_block' => false,
+				'frontend_available' => true,
+			]
+		);
+
+		$this->end_controls_section();
+
+        //TODO: Add configuration from siema api
 
         $this->end_controls_section();
     }
